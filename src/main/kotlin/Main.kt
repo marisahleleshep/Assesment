c1790b65d393272a2ee6ff581a11e77f2df54a82
marisahleleshep <leleshepmmarisaa@gmail.com>
2023-03-09 08:46:33 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main(){
+information("Marisah")
+    numbers(3,)
+    friends("Mike")
+
+
+
+
+}
+fun information(names:String){
+    var names=("Marisah Ann")
+    println(names[1])
+
+
+}
+fun numbers(num:Int){
+    var letters= arrayOf(12,7,4,4,7)
+    println(letters.sortedArray())
+    return
+
+}
+
+fun friends(names:String){
+    var names= arrayOf("Mike","Ken","Anne")
+    println(arrayOf(names.sortedArray()))
+
+}
+class calculator(num1:Int,num2:Int){
+
+}
